92b822a9d9a838d8b4087abff8efa8825e00ebbb
Varun Jain <vj986725@gmail.com>
2025-11-23 10:02:32 +0530

    changes in backend/server.js

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,7 @@
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
-import { getDocHTML, getSheet, getDriveFiles } from "./google.js";
+import { getSheet, getDriveFiles, getDocWithMeta } from "./google.js";
 
 dotenv.config();
 
